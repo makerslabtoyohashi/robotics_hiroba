21e9c2f98d27c13f0e2598f90f152aff489a2327
B163301 <m.t.arigato.reine@gmail.com>
2019-05-10 14:21:47 +0900

    画像を受け取り保存するリクエストプログラムを確認。いらないファイルを削除

--- src/MyClient.py
+++ src/MyClient.py
@@ -1,23 +1,30 @@
 import urllib.request
 
+
 class MyClient:
-    def __init__(self, ipaddress, port, page = ''):
+    def __init__(self, ipaddress, port, page=''):
         self.ipaddress = ipaddress
         self.page = page
         self.port = port
+
     def _create_request(self, params):
         url = 'http://{}:{}'.format(self.ipaddress, self.port)
-        if len(self.page)>0:
+        if len(self.page) > 0:
             url = url + self.page
         req = urllib.request.Request('{}?{}'.format(url, urllib.parse.urlencode(params)))
         return req
+
     def send(self, params):
         req = self._create_request(params)
+        body = None
         with urllib.request.urlopen(req) as res:
             body = res.read()
-            print('recv :', body)
+        return body
 
-#if __name__=='__main__':
-#    client = MyClient('192.168.2.103', '12345')
-#    params = {'foo':'123'}
-#    client.send(params)
+
+if __name__ == '__main__':
+    client = MyClient('192.168.24.63', '1880', page='/getimg')
+    params = {}
+    body = client.send(params)
+    with open('test.jpeg', 'wb') as f:
+        f.write(body)
